90b992d00aec28bc0068b68015bf942dc95c4039
Clément Murzeau <clement@studiorelief.fr>
2023-04-24 18:32:07 +0200

    build for test web3modal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import { greetUser } from '$utils/greet';
+import { configureChains, createClient } from '@wagmi/core';
+import { arbitrum, mainnet, polygon } from '@wagmi/core/chains';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { Web3Modal } from '@web3modal/html';
 
-window.Webflow ||= [];
-window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+const chains = [arbitrum, mainnet, polygon];
+const projectId = 'web3_pyratz';
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  provider,
 });
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+const web3modal = new Web3Modal({ projectId }, ethereumClient);
